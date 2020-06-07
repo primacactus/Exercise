b7acd5e896d5d64833e8f52c2aac51308400b891
Liubov Rodak <primacactus@gmail.com>
2020-06-06 18:11:20 -0600

    API test for currency rates

--- README.md
+++ README.md
@@ -4,22 +4,27 @@
 Sample API test.
 
 # Implementation
-Written in Python3.
+Written in Python 3.8.3.
+
 Libraries used: 
 * requests
 * pytest
+* pytest-html
 
 # Test details
-Endpoint tested: https://cat-fact.herokuapp.com/
-The app sends daily cat facts. Test checks if 1st received fact contains "cat" word.
-No auth required to get cat facts.
+For this exercise I selected https://exchangeratesapi.io/. This API provides exchange rates, latest and historical.
+
+I'm checking a number of positive scenarios and some error cases. There are also data-driven tests where I check rate value for historic data.
+
+Given endpoint does not require an API key or any other way of authentication.
+
+You may notice there are not much checks of date value in responses. The reason is when requested date is a weekend or holiday the endpoint returns rates for the last business day. For a real test I'd account for this logic: find a source of business days for the given business institution and check such cases as weekends and last and first day of the year. For this exercise I left this out of scope.
+
+The test "test_special_case_will_fail" fails, as it's name implies. It checks a special case, when I request an exchange rate for the currency itself. EUR:EUR and USD:USD are tested. The endpoint acts inconsistently here: for USD:USD if gives 1.0, but for EUR:EUR gives an error code. This could happen because EUR is the default base here.
+
+As this is a 3-rd party website, I do not include any load or failover testing. You can find an example of a performance test where I'm logging responce times for a certain number af currency symbols (1, 2, 4, 8) and increasing date interval (1 day, 1 month, 1 year). In real world I would feed this data to ELK stack and supplement it with server performance data on a dashboard.
 
 # How to run
-Run `pytest` in the test directory.
-Sample output:
+Run `pytest --html=report.html` in the test directory.
 
-    collected 1 item
-    
-    test_api.py .                                                                                                    [100%]
-    
-    ================================================== 1 passed in 3.66s ==================================================
+Sample output: [report.html](report.html) (note 1 failing test).
--- test_api.py
+++ test_api.py
@@ -1,18 +1,77 @@
 import requests
+import csv
+import pytest
+import datetime
 
-api_url_base = 'https://cat-fact.herokuapp.com/'
-
-headers = {'Content-Type': 'application/json'}
+api_url_base = 'https://api.exchangeratesapi.io/'
 
 
-def test_get_cat_facts():
+def read_test_data(filename):
+    test_data = []
+    with open(f'test_data/{filename}', newline='') as df:
+        reader = csv.reader(df, delimiter=',')
+        next(reader)
+        for row in reader:
+            test_data.append(row)
+    return test_data
 
-    cat_facts = '{0}facts'.format(api_url_base)
-    response = requests.get(cat_facts, headers)
+
+def get_rates(path, **params):
+    rates = f'{api_url_base}{path}'
+    response = requests.get(rates, params=params)
+    return response
+
+
+def test_get_last_rate():
+    response = get_rates("latest")
     assert response.status_code == 200
-
+    assert response.headers['Content-Type'] == "application/json"
     data = response.json()
-    assert 'cat' in data['all'][0]['text'].lower()
+    assert data['base'] == 'EUR'
+    for rate in data['rates'].values():
+        assert isinstance(rate, float)
 
-if __name__ == "__main__":
-    test_get_cat_facts()
+
+def test_get_rate_for_symbols():
+    symbols = ['CAD', 'USD', 'HUF', 'RUB', 'PLN', 'KRW', 'GBP', 'HKD', 'DKK']
+    response = get_rates("latest", symbols=','.join(symbols))
+    assert response.status_code == 200
+    data = response.json()
+    for symbol in symbols:
+        assert isinstance(data['rates'][symbol], float)
+
+
+@pytest.mark.parametrize("date, base, symbol, rate", read_test_data("historic_data.csv"))
+def test_get_historic_rates(date, base, symbol, rate):
+    response = get_rates(date, base=base, symbols=symbol)
+    assert response.status_code == 200
+    data = response.json()
+    assert str(data['rates'][symbol]) == rate
+
+
+@pytest.mark.parametrize("path, base, symbol", read_test_data("special_case_data.csv"))
+def test_special_case_will_fail(path, base, symbol):
+    response = get_rates(path, base=base, symbols=symbol)
+    assert response.status_code == 200
+    data = response.json()
+    assert str(data['rates'][symbol]) == '1.0'
+
+
+def test_no_such_symbol():
+    symbol = 'AAA'
+    response = get_rates("latest", symbols=symbol)
+    assert response.status_code == 400
+
+
+def test_date_is_too_old():
+    date = '1990-02-02'
+    response = get_rates(date)
+    assert response.status_code == 400
+
+
+def test_date_range_in_the_future():
+    start_at = '{0:%Y-%m-%d}'.format(datetime.datetime.now() + datetime.timedelta(1))
+    end_at = '{0:%Y-%m-%d}'.format(datetime.datetime.now() + datetime.timedelta(7))
+    response = get_rates("history", start_at=start_at, end_at=end_at)
+    assert response.status_code == 200
+    assert not response.json()['rates']
